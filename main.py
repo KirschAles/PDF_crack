0c47139203f5a3489554714cb354925f1d648341
Ales Kirsch <kirschales@gmail.com>
2023-08-10 22:03:12 +0200

    Made Basic script

---
+++ main.py
@@ -0,0 +1,15 @@
+from pypdf import PdfReader
+import sys
+
+
+def main():
+    filename = sys.argv[1]
+    reader = PdfReader(filename)
+    for num in range(10000):
+        a = reader.decrypt("{0:04d}".format(num))
+        if a != 0:
+            print(num)
+
+
+if __name__ == '__main__':
+    main()
