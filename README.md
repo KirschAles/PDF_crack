d965464673e93755cf81b93d24a0f4ee0fe6e6d5
Kirsch Aleš <kirschales@gmail.com>
2023-12-11 12:46:37 +0100

    Can now deal with letters

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Usage
-main.py \<path to pdf file\>
+# PDF CRACK
+This is an extremely simple program for getting passwords of locked PDFs.
+
+## Usage
+`python main.py [flags] <path to pdf>`
+
+### Flags
+* **-n** or **--numbers**: password can contain numbers
+* **-a** or **--alphabet**: password can contain english alphabet
+* **-l** or **--length**: length of the password, is 4 by default
+
+### Examples of Usage
+* `python main.py -n file.pdf`
+* `python main.py -n -a -l=5 my_file.pdf`
--- main.py
+++ main.py
@@ -1,15 +1,43 @@
 from pypdf import PdfReader
-import sys
+from argparse import ArgumentParser
+from string import digits, ascii_lowercase, ascii_uppercase
+from itertools import product
+
+parser = ArgumentParser()
+parser.add_argument('filename',
+                    help='Path to the pdf file')
+parser.add_argument('-n', '--numbers',
+                    default=False,
+                    const=True,
+                    nargs='?',
+                    help='Password uses numbers.')
+parser.add_argument('-a', '--alphabet',
+                    default=False,
+                    const=True,
+                    nargs='?',
+                    help='Password uses alphabet.')
+parser.add_argument('-l', '--length',
+                    default=4,
+                    help='Length of password.')
 
 
-def main():
-    filename = sys.argv[1]
+def main(args):
+    filename = args.filename
+
+    used_letters = ''
+    if args.numbers:
+        used_letters += digits
+    if args.alphabet:
+        used_letters += ascii_uppercase + ascii_lowercase
+
     reader = PdfReader(filename)
-    for num in range(10000):
-        a = reader.decrypt('{0:04d}'.format(num))
+    for permutation in product(used_letters, repeat=args.length):
+        permutation = ''.join(permutation)
+        a = reader.decrypt(permutation)
         if a != 0:
-            print('Password is: {0:04d}'.format(num))
+            print('Password is: {}'.format(permutation))
+            break
 
 
 if __name__ == '__main__':
-    main()
+    main(parser.parse_args())
